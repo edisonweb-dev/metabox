b0c1415fffd187d8fd34f1f908fad4366dfafffa
Edison Perdomo <edisonperdomodev@gmail.com>
2020-03-25 21:15:38 -0400

    Creando la funcion de guardado del metabox

--- ga-metaboxes.php
+++ ga-metaboxes.php
@@ -50,3 +50,37 @@ function ga_diseno_metaboxes($post) {
      </div>
   <?php
 }
+
+
+function ga_guardar_metaboxes($post_id, $post, $update) {
+  if (!isset($_POST["meta-box-nonce"]) || !wp_verify_nonce($_POST["meta-box-nonce"], basename(__FILE__)))
+      return $post_id;
+
+  if(!current_user_can("edit_post", $post_id))
+      return $post_id;
+
+  if(defined("DOING_AUTOSAVE") && DOING_AUTOSAVE)
+      return $post_id;
+
+  $input_metabox = "";
+  $textarea_metabox = "";
+  $dropdown_metabox = "";
+
+  if(isset($_POST["input-metabox"])) {
+      $input_metabox = $_POST["input-metabox"];
+  }
+  update_post_meta($post_id, "input-metabox", $input_metabox);
+
+  if(isset($_POST["textarea-metabox"])) {
+      $textarea_metabox = $_POST["textarea-metabox"];
+  }
+  update_post_meta($post_id, "textarea-metabox", $textarea_metabox);
+
+
+  if(isset($_POST["dropdown-metabox"])) {
+      $dropdown_metabox = $_POST["dropdown-metabox"];
+
+  }
+    update_post_meta($post_id, "dropdown-metabox", $dropdown_metabox);
+}
+add_action("save_post", "ga_guardar_metaboxes", 10, 3);
